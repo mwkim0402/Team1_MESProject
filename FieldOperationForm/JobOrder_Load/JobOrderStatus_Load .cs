4680782460b01ba2eb0ed3a560fa7c8a2fa8ef85
wldms111 <852387@naver.com>
2019-12-31 18:13:44 +0900

    1231 지은

---
+++ FieldOperationForm/JobOrder_Load/JobOrderStatus_Load .cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FieldOperationForm
+{
+    public partial class JobOrderStatus_Load : Form
+    {
+        Main_P main;
+
+        public JobOrderStatus_Load(Main_P main1)
+        {
+            InitializeComponent();
+            main = main1;
+        }
+
+        private void JobOrderStatus_Load_Load(object sender, EventArgs e)
+        {
+            main.lblChange.Text = "작업지시 현황";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            workerAllocation frm = new workerAllocation(main);
+            frm.BringToFront();
+            frm.MdiParent = main;
+            frm.Dock = DockStyle.Fill;
+            frm.Show();
+            main.lblChange.Text = "작업자할당";
+           
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            ProcessCondition frm = new ProcessCondition(main);
+            frm.BringToFront();
+            frm.MdiParent = main;
+            frm.Dock = DockStyle.Fill;
+            frm.Show();
+            main.lblChange.Text = "공정조건 등록";
+        }
+    }
+}
---
+++ FieldOperationForm/JobOrder_Molding/JobOrderStatus .cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FieldOperationForm
+{
+    public partial class JobOrderStatus : Form
+    {
+        Main_P main;
+
+        public JobOrderStatus(Main_P main1)
+        {
+            InitializeComponent();
+            main = main1;
+        }
+
+        private void JobOrderStatus_Load(object sender, EventArgs e)
+        {
+            main.lblChange.Text = "작업지시 현황";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            workerAllocation frm = new workerAllocation(main);
+            frm.BringToFront();
+            frm.MdiParent = main;
+            frm.Dock = DockStyle.Fill;
+            frm.Show();
+            main.lblChange.Text = "작업자할당";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ProcessCondition frm = new ProcessCondition(main);
+            frm.BringToFront();
+            frm.MdiParent = main;
+            frm.Dock = DockStyle.Fill;
+            frm.Show();
+            main.lblChange.Text = "공정조건 등록";
+        }
+    }
+}
