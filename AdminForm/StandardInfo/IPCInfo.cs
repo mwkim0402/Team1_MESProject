a304c1e43f7c2edc9f2e9bb4ffe4a736f40e2078
Haechan <rkdgocks@naver.com>
2020-01-02 18:14:19 +0900

    01.02

---
+++ AdminForm/StandardInfo/IPCInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AdminForm.StandardInfo
+{
+    public partial class IPCInfo : AdminForm.dgvOneWithInput
+    {
+        public IPCInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
