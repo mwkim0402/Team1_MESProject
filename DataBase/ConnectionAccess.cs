6f53d4ed62147a9bf573843c70f0609b948471b1
mwkim0402 <mwkim0402@naver.com>
2020-01-02 09:42:16 +0900

    MainForm 메뉴 MES_DB 생성

---
+++ DataBase/ConnectionAccess.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+using System.Reflection;
+
+namespace DataBase
+{
+    public abstract class ConnectionAccess
+    {
+        protected string ConnectionString
+        {
+            get
+            {
+                string connStr = string.Empty;
+                XmlDocument configXml = new XmlDocument();
+                configXml.Load(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/DataBase_XML.xml");
+                XmlNodeList addNodes = configXml.SelectNodes("configuration/settings/add");
+                foreach (XmlNode xmlNode in addNodes)
+                {
+                    if (xmlNode.Attributes["key"].InnerText == "MyDB")
+                    {
+                        connStr = ((XmlCDataSection)xmlNode.ChildNodes[0]).InnerText;
+                        break;
+                    }
+                }
+                return connStr;
+            }
+        }
+    }
+}
