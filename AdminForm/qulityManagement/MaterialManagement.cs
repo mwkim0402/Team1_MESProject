098c49b2bfe1fb94c7773d5250b235c11960f652
Haechan <rkdgocks@naver.com>
2020-01-03 16:55:26 +0900

    last test

---
+++ AdminForm/qulityManagement/MaterialManagement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AdminForm.qulityManagement
+{
+    public partial class MaterialManagement : AdminForm.dgvOne
+    {
+        public MaterialManagement()
+        {
+            InitializeComponent();
+        }
+    }
+}
